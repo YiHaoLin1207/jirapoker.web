7121000dea627ca2fa2da6084553046ea5a9d8e8
Limin Chien (簡立銘) <liminchien@cybersoft4u.com.tw>
2020-01-08 18:14:48 +0800

    VUE: new component: clicableAvatar

---
+++ jirapoker.web/src/components/JiraPoker/components/clickableAvatar.vue
@@ -0,0 +1,24 @@
+<template>
+<div @click="handleClick">
+  <Tooltip :content="user.userName" placement="bottom">
+    <Avatar :src="user.avatarUrl" />
+  </Tooltip>
+</div>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from 'vue';
+import { UserProfile } from '@/classes/model';
+
+export default Vue.extend({
+  name: 'clickableAvatar',
+  props: { user: Object, },
+  methods: {
+    handleClick() { 
+      this.$emit('click');
+    },
+  },
+  created() { console.log("Created"); }
+
+});
+</script>
