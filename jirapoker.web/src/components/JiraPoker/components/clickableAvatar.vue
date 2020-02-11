b7a50d7acd8ab982b74693f0b6293c0096e147b4
limitimil <zlsh10340@gmail.com>
2020-02-11 15:05:03 +0800

    VUE: highlight clickable avatar

--- jirapoker.web/src/components/JiraPoker/components/clickableAvatar.vue
+++ jirapoker.web/src/components/JiraPoker/components/clickableAvatar.vue
@@ -1,5 +1,5 @@
 <template>
-<div @click="handleClick">
+<div class="clickable-avatar" @click="handleClick" :style="style">
   <Tooltip :content="user.userName" placement="bottom">
     <Avatar :src="user.avatarUrl" />
   </Tooltip>
@@ -13,12 +13,24 @@ import { UserProfile } from '@/classes/model';
 export default Vue.extend({
   name: 'clickableAvatar',
   props: { user: Object, },
+  data() {
+    return {
+      style: {} as any,
+    };
+  },
   methods: {
     handleClick() { 
       this.$emit('click');
+      this.$set(this.style,'background-color','#A7FFF6');
     },
   },
   created() { console.log("Created"); }
 
 });
 </script>
+<style scoped lang="less">
+.clickable-avatar {
+  padding: 3px;
+  background-color: red;
+}
+</style>
