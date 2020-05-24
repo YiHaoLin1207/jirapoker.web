e7dba17202496ee4199a0659202a59891cb71447
limitimil <liminchien@cybersoft4u.com>
2020-05-24 23:28:44 +0800

    MODEL: story point

---
+++ jirapoker.web/src/classes/model/StoryPoint.ts
@@ -0,0 +1,4 @@
+export default class StoryPoint {
+    public static storyPoints : string[]= ['0', '0.5', '1', '2', '3', '5', '8', '13', '21', '34', '?']; 
+}
+
--- jirapoker.web/src/classes/model/index.ts
+++ jirapoker.web/src/classes/model/index.ts
@@ -1,8 +1,16 @@
 import LocaleMsg from './LocaleMsg';
 import MenuNode from './MenuNode';
 import MenuNodeMeta from './MenuNodeMeta';
+import StoryPoint from './StoryPoint';
 import TransferModel from './TransferModel';
 import UserProfile from './UserProfile';
 
-export { MenuNode, MenuNodeMeta, LocaleMsg, UserProfile, TransferModel };
+export {
+  LocaleMsg,
+  MenuNode,
+  MenuNodeMeta,
+  StoryPoint,
+  TransferModel,
+  UserProfile,
+};
 
