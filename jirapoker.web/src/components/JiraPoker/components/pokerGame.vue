cee8baba5e5870c4f8182d547aba06d3fefb082d
limitimil <liminchien@cybersoft4u.com>
2020-05-24 22:51:17 +0800

    VUE: new compoents poker game

---
+++ jirapoker.web/src/components/JiraPoker/components/pokerGame.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="hack">Hello World</div>
+</template>
+<script lang="ts">
+import Vue, { PropType } from 'vue';
+export default Vue.extend({
+  name: 'JiraPoker', 
+});
+</script>
+<style lang="less" scoped>
+</style>
